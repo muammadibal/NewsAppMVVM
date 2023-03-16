7cd8e286b9918b356b278fb27124c6e15ee65061
ibal <muammadibal@gmail.com>
2023-03-16 14:55:47 +0700

    create retrofit instance request

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "19946e7c242b4d2da3e99c6267de4ad3"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
